8d804657fe1ef859b521118dfe78942e0670686f
Eojin Kim <eojin1211@hanmail.net>
2024-03-30 11:52:04 +0000

    Vite Node 플러그인 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+if (import.meta.env.PROD) {
+  (async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  })();
 }
-bootstrap();
+
+export const viteNodeApp = NestFactory.create(AppModule);
