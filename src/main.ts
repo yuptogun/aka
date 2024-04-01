89ebfe3f66a2498ac2312ea205b162f1bb866039
yuptogun <eojin1211@hanmail.net>
2024-04-01 03:16:17 +0000

    진행 방향 조정에 따른 일부 원복
    - 27e09bf
    - 8d80465

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-if (import.meta.env.PROD) {
-  (async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
-  })();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-
-export const viteNodeApp = NestFactory.create(AppModule);
+bootstrap();
