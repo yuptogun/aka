ead4ec097ff8f2b63eb32ff8f731abb1cb0a195a
Eojin Kim <eojin1211@hanmail.net>
2024-04-01 10:08:36 +0000

    @nestjs/config 설치

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!'; // `Hello World from ${process.env.DATABASE_HOST + ':' + process.env.DATABASE_PORT}!`;
   }
 }
