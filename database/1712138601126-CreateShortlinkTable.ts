7026fef95c2d09f0b4c72f3630a49a2318d89d8a
Eojin Kim <eojin1211@hanmail.net>
2024-04-05 11:08:07 +0000

    shortlinks 엔티티/마이그레이션 정의

---
+++ database/1712138601126-CreateShortlinkTable.ts
@@ -0,0 +1,63 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateShortlinkTable1712138601126 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'shortlink',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'url',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'code',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'update_token',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'saved_from',
+            type: 'inet4',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('shortlinks');
+  }
+}
---
+++ src/entities/shortlink.ts
@@ -0,0 +1,43 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Shortlink {
+  /** PK */
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  /** 사용자가 줄이고자 하는 URL */
+  @Column('varchar')
+  url: string;
+
+  /** URL에 고유 할당한 코드. aka.eojin.dev/~ 자리에 들어가게 됨 */
+  @Column('varchar')
+  code: string;
+
+  /** 다음 수정을 위한 1회용 인증토큰. 로그인 없이 authorization 구현하기 위함 */
+  @Column('varchar')
+  update_token: string;
+
+  /** 최종 저장한 사람의 IP. 로그인 없이 rate limit 구현하기 위함 */
+  @Column('inet4')
+  saved_from: number;
+
+  /** 최초생성일시 */
+  @CreateDateColumn()
+  created_at: Date;
+
+  /** 최종수정일시 */
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  /** "삭제"일시 */
+  @DeleteDateColumn()
+  deleted_at: Date | null;
+}
