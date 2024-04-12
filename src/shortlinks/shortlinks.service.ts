4d28cbd7ded52144c6616bda57288c167a936139
yuptogun <eojin1211@hanmail.net>
2024-04-12 06:16:23 +0000

    Create 구현 Fixes #5

--- src/shortlinks/shortlinks.service.ts
+++ src/shortlinks/shortlinks.service.ts
@@ -4,6 +4,9 @@ import { UpdateShortlinkDto } from './dto/update-shortlink.dto';
 import { Repository } from 'typeorm';
 import { Shortlink } from './entities/shortlink.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { getRandomValues, randomBytes } from 'crypto';
+
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 @Injectable()
 export class ShortlinksService {
@@ -12,13 +15,30 @@ export class ShortlinksService {
     private repo: Repository<Shortlink>,
   ) {}
 
-  create(input: CreateShortlinkDto) {
-    return input;
+  async create(body: CreateShortlinkDto, ip: string) {
+    let shortlink = await this.repo.findOneBy({
+      deleted_at: null,
+      url: body.url,
+    });
+    if (shortlink === null) {
+      const newShortlink = new Shortlink();
+      newShortlink.url = body.url;
+      newShortlink.code = await this.getRandomCode();
+      newShortlink.update_token = randomBytes(20).toString('hex');
+      newShortlink.saved_from = ip;
+      shortlink = await this.repo.save(newShortlink);
+    }
+    return shortlink;
   }
 
   findAll() {
-    return this.repo.findBy({
-      deleted_at: null,
+    return this.repo.find({
+      where: {
+        deleted_at: null,
+      },
+      order: {
+        id: 'desc',
+      },
     });
   }
 
@@ -32,5 +52,37 @@ export class ShortlinksService {
 
   remove(id: number) {
     return `This action removes a #${id} shortlink`;
+  }
+
+  async getRandomCode(len: number = 3): Promise<string> {
+    const maxTrial = chars.length ** len;
+    let trial = 0;
+    let code: string;
+    let shouldRetry = true;
+    do {
+      if (trial === maxTrial) {
+        code = await this.getRandomCode(++len);
+        shouldRetry = false;
+      } else {
+        trial++;
+        code = this.createRandomCode(len);
+        shouldRetry = await this.repo.existsBy({
+          deleted_at: null,
+          code: code,
+        });
+      }
+    } while (shouldRetry);
+
+    return code;
+  }
+
+  createRandomCode(length: number) {
+    let result = '';
+    const randomArray = new Uint8Array(length);
+    getRandomValues(randomArray);
+    randomArray.forEach((number) => {
+      result += chars[number % chars.length];
+    });
+    return result;
   }
 }
