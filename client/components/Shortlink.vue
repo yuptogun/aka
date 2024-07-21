138576c9bcc17ef478bbc4cc5ea3ea343dc7b56e
yuptogun <eojin1211@hanmail.net>
2024-07-21 07:53:33 +0000

    delete basically done

--- client/components/Shortlink.vue
+++ client/components/Shortlink.vue
@@ -1,22 +1,51 @@
 <script setup lang="ts">
-const state = reactive({
+const stateInitial = {
   url: '',
-  title: null
-})
+  title: null,
+  id: null
+}
+const state = reactive({ ...stateInitial })
 const shortlink = ref()
 
 async function onSubmit(event) {
   shortlink.value = null
-  const response = await $fetch('/server/shortlinks', {
+  await fetch('/server/shortlinks', {
     method: 'POST',
     headers: [
       ['Content-Type', 'application/json'],
       ['Accept', 'application/json'],
     ],
-    body: event.data
+    body: JSON.stringify(event.data)
+  }).then(async (res) => {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    shortlink.value = await res.json()
+    Object.assign(state, shortlink.value)
+  }).catch((e: Error) => {
+    alert(e.message)
   })
-  console.log(response)
-  shortlink.value = response
+}
+
+async function onDelete() {
+  if (!state.id) {
+    alert('you can delete only what you just created!')
+    return
+  }
+  if (!confirm('you sure?')) {
+    return
+  }
+  await fetch(`/server/shortlinks/${state.id}`, {
+    method: 'DELETE'
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    shortlink.value = null
+    Object.assign(state, stateInitial)
+  }).catch((e: Error) => {
+    alert(e.message)
+  })
 }
 </script>
 
@@ -31,7 +60,7 @@ async function onSubmit(event) {
         <UFormGroup label="Page title (optional)" name="url">
           <UInput type="text" v-model="state.title" />
         </UFormGroup>
-        <UButton type="submit" color="gray">
+        <UButton type="submit" color="primary">
           Create
         </UButton>
       </UForm>
@@ -42,6 +71,9 @@ async function onSubmit(event) {
         <UInput type="url" class="w-auto inline-block cursor-pointer" size="xl" aria-readonly="true" readonly
           :value="`https://aka.eojin.dev/${shortlink.code}`"
           @click="(e) => {e.target.focus(); e.target.select()}"></UInput>
+        <p>
+          <UButton type="button" color="red" @click="onDelete">Delete</UButton>
+        </p>
       </div>
     </UCard>
   </div>
